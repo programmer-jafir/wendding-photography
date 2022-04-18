45e3f8ad456263e82a8fcf8d351c705d17179f8a
programmer-jafir <jafiranjum2020@gmail.com>
2022-04-18 11:58:27 +0600

    login and register athentication added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import Regester from './Pages/Register/Register';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
     <Route path='/home' element={<Home/>}/>
     <Route path='/blogs' element={<Blogs/>}/>
     <Route path='login' element={<Login/>}/>
-    <Route path='/register' element={<Regester></Regester>}/>
+    <Route path='/register' element={<Register></Register>}/>
       </Routes>
     </div>
   );
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey, 
-  authDomain:process.env.REACT_APP_authDomain, 
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyCL9JBUWj9wNpCDwY01a7UqpJRDgKfd2JU",
+  authDomain: "independent-service-prov-2e5d7.firebaseapp.com",
+  projectId: "independent-service-prov-2e5d7",
+  storageBucket: "independent-service-prov-2e5d7.appspot.com",
+  messagingSenderId: "402609636909",
+  appId: "1:402609636909:web:fff76e8cf1f6d3e041daa4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
+
 
 export default auth;
