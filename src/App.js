82394216746146dba9522ba5fd50574806ff820f
programmer-jafir <jafiranjum2020@gmail.com>
2022-04-18 09:55:28 +0600

    Login ui added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Regester from './Pages/Register/Register';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
     <Route path='/' element={<Home/>}/>
     <Route path='/home' element={<Home/>}/>
     <Route path='/blogs' element={<Blogs/>}/>
+    <Route path='login' element={<Login/>}/>
+    <Route path='/register' element={<Regester></Regester>}/>
       </Routes>
     </div>
   );
