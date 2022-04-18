50eb37ad33df76487c42118203b7541e0c243f23
programmer-jafir <jafiranjum2020@gmail.com>
2022-04-18 17:20:08 +0600

    private route added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,18 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import logo from '../../../img/logo.png'
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+
+  const handleSignOut = () =>{
+    signOut(auth);
+  }
+
     return (
         <>
               <Navbar collapseOnSelect expand="lg" sticky='top' bg="light" variant="light">
@@ -20,9 +29,13 @@ const Header = () => {
     </Nav>
     <Nav>
       <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
-      <Nav.Link as={Link} to="login">
+      {
+        user ?
+        <button className='btn btn-link text-dark text-decoration-none' onClick={handleSignOut}>Sign out</button>
+        :
+        <Nav.Link as={Link} to="login">
         Login
-      </Nav.Link>
+      </Nav.Link>}
     </Nav>
   </Navbar.Collapse>
   </Container>
